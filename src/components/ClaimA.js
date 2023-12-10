655a4960888dcb7583f4f1010331a2065c32f86c
Justin Tam <tamtzekin@gmail.com>
2023-12-10 22:43:26 +0000

    remove unused import

--- src/components/ClaimA.js
+++ src/components/ClaimA.js
@@ -2,7 +2,6 @@ import logo from '../logo.svg';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './ClaimPage.css';
-import './Arrow.css';
 import VideoPlayer from './VideoPlayer';
 
 const ClaimA = () => {

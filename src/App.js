1cf7ded4233c9cc35e6df219e4e09d6438da5695
Justin Tam <tamtzekin@gmail.com>
2023-12-10 21:56:00 +0000

    repush bc deployment isnt working

--- src/App.js
+++ src/App.js
@@ -16,8 +16,6 @@ import VideoPlayer from './components/VideoPlayer';
 
 const App = () => {
 
-    // const videoLink = 'https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4'; // Replace with the actual path to your video
-
     return (
     <>
             <Router>

9b6429efdaaa0f49ac1140b159c725c0095fce54
Justin Tam <tamtzekin@gmail.com>
2023-12-11 23:36:26 +0000

    volunteer form

--- src/components/VolunteerForm.js
+++ src/components/VolunteerForm.js
@@ -1,21 +1,144 @@
+import React, { useState } from 'react';
 import '../App.css';
+import './VolunteerForm.css';
 
 const VolunteerForm = () => {
-    return (
-        <>
-        <span class="header-container">
-            <h1>Hasbara Tracker</h1>
-                <span id="dots">. . . . . . . . . . . . . . .</span>
-        </span>
+  const [formData, setFormData] = useState({
+    fullName: '',
+    email: '',
+    backgroundAndSkills: '',
+    hoursCommitted: '',
+    otherAmountOfHours: '',
+  });   
 
-        <span className='content-container'>
-            <h2>Volunteer</h2>
-                Volunteer to help build a database tracking and debunking fabrications by the Israeli state during its genocidal campaign against Palestinians in Gaza since 7 October 2023.<br />
-                <br />
-                We will task volunteers with a specific claim and provide more information about how to document and archive. Anyone can help — especially if they have a propensity for research and collecting receipts.
-        </span>
-        </>
-    )
-}
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle the form submission logic here, e.g., send data to a server
+    console.log('Form submitted:', formData);
+    // You can add further logic, such as sending the data to a server or clearing the form fields
+    setFormData({
+      fullName: '',
+      email: '',
+      backgroundAndSkills: '',
+      hoursCommitted: '',
+      otherAmountOfHours: '',
+      languageSkill: '',
+    });
+  };
+
+  return (
+    <>
+      <span className="header-container">
+        <h1>Hasbara Tracker</h1>
+        <span id="dots">. . . . . . . . . . . . . . .</span>
+      </span>
+
+      <div className="content-container">
+        <h2>Volunteer</h2>
+        <p>
+          Volunteer to help build a database tracking and debunking fabrications
+          by the Israeli state during its genocidal campaign against Palestinians
+          in Gaza since 7 October 2023.
+        </p>
+        <p>
+          We will task volunteers with a specific claim and provide more
+          information about how to document and archive. Anyone can help —
+          especially if they have a propensity for research and collecting
+          receipts.
+        </p>
+        <br />
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            Full name *
+            <br />
+            <input
+              type="text"
+              name="fullName"
+              value={formData.fullName}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <br />
+
+          <label>
+            Email *
+            <br />
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </label>
+          <br />
+          <label>
+
+            Tell us a little bit about your background and skills so we can match you with the right task. * 
+            <br />
+            <textarea
+              name="backgroundAndSkills"
+              value={formData.backgroundAndSkills}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+
+          <label>
+            How many hours can you commit?
+            <br />
+            <select
+              name="hoursCommitted"
+              value={formData.hoursCommitted}
+              onChange={handleChange}
+            >
+              <option value="">Select</option>
+              <option value="1">1 hour</option>
+              <option value="1-3">1 -3 hours</option>
+              <option value="3-5">3 - 5 hours</option>
+              <option value="other">Other</option>
+            </select>
+          </label>
+          <br />
+
+          {formData.hoursCommitted === 'other' && (
+          <label>
+            Enter an amount of hours.
+            <br />
+            <input
+              type="text"
+              name="otherAmountOfHours"
+              value={formData.otherAmountOfHours}
+              onChange={handleChange}            />
+          </label>
+          )}
+
+          <br />
+          <label>
+            This isn’t necessary, but do you understand Arabic and/or Hebrew? Let us know what level. 
+            <input
+              type="text"
+              name="languageSkill"
+              value={formData.languageSkill}
+              onChange={handleChange}
+            />
+          </label>
+          <br />
+          <button class="green-button" type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default VolunteerForm;

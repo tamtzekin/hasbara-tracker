3ffac9d5ad73293471fd1067d0e99dd0f533e216
Justin Tam <tamtzekin@gmail.com>
2023-12-12 00:53:35 +0000

    neatened homepage css

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -17,24 +17,23 @@ const Homepage = () => {
 
 <span class="header-container">
             <h1>Hasbara Tracker</h1>
-                <span id="dots">. . . . . . . . . . . . . .</span>
-        </span>
+            <span id="dots">. . . . . . . . . . . . . .</span>
+</span>
         
-
         <span className="content-container">
-        <br />
+            <br />
+
             <h2>About</h2><br />
             
-                <span className="home-text">We’re working on documenting and debunking claims made by the Israeli regime during its most recent – and unprecedented – genocidal campaign in Gaza since 7 Oct 2023. The Israeli settler-colony continues to spread disinformation to manufacture consent for ethnic cleansing and land theft, as it has since the Nakba. Our aim is to centralise this information.</span>
+            <span className="home-text">We’re working on documenting and debunking claims made by the Israeli regime during its most recent – and unprecedented – genocidal campaign in Gaza since 7 Oct 2023. The Israeli settler-colony continues to spread disinformation to manufacture consent for ethnic cleansing and land theft, as it has since the Nakba. Our aim is to centralise this information.</span>
 
-        <h2>Volunteer on this project</h2><br />
+            <h2>Volunteer on this project</h2><br />
 
-        <span className="home-text">Volunteer to help build a database tracking and debunking fabrications by the Israeli state during its current genocidal campaign against Palestinians in Gaza since 7 October 2023. We will task each volunteer with a specific claim and provide more information about how to document and archive. Anyone can help — especially if they have a propensity for research and collecting receipts.</span><br />
-        <br />
-        <br />
+            <span className="home-text">Volunteer to help build a database tracking and debunking fabrications by the Israeli state during its current genocidal campaign against Palestinians in Gaza since 7 October 2023. We will task each volunteer with a specific claim and provide more information about how to document and archive. Anyone can help — especially if they have a propensity for research and collecting receipts.</span><br />
+            <br />
+            <br />
 
-        <Link to="/volunteer"><div className="green-button">Help out</div></Link>
-
+            <Link to="/volunteer"><div className="green-button">Help out</div></Link>
         </span>
 
     </>

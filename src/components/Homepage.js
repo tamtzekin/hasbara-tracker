b5d45363d46a1276cd912732dfb49b9f579fe3c3
Justin Tam <tamtzekin@gmail.com>
2023-12-17 22:07:33 +0000

    lots of style changes, h2 font

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -23,8 +23,7 @@ const Homepage = () => {
 </span>
         
         <span className="content-container">
-            <br />
-
+        
             <h2>About</h2><br />
             
             <span className="home-text">We’re working on documenting and debunking claims made by the Israeli regime during its most recent – and unprecedented – genocidal campaign in Gaza since 7 Oct 2023. The Israeli settler-colony continues to spread disinformation to manufacture consent for ethnic cleansing and land theft, as it has since the Nakba. Our aim is to centralise this information.</span>

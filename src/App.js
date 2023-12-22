764a73d16c867a73d44a63088df118f680441304
Justin Tam <tamtzekin@gmail.com>
2023-12-22 22:46:14 +0000

    clean up old components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import logo from './logo.svg';
 import './App.css';
 
 import Homepage from './components/Homepage';
-import About from './components/About';
 // import NavBar from './components/NavBar';
 import ClaimsDirectory from './components/ClaimsDirectory';
 import VolunteerForm from './components/VolunteerForm';
@@ -22,7 +20,6 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Homepage />} />
                     <Route path="/homepage" element={<Homepage />} />
-                    <Route path="/about" element={<About />} />
                     <Route path="/claims" element={<ClaimsDirectory />} />
                     <Route path="/volunteer" element={<VolunteerForm />} />
                     <Route path="/tracker" element={<Tracker />} />

8f6e608b9395781fb3272ef3f20f5b8522c63d83
Justin Tam <tamtzekin@gmail.com>
2023-12-22 20:50:34 +0000

    unused drafts

--- src/components/FetchNotionData.js
+++
@@ -1,5 +0,0 @@
-import { NotionAPI } from 'notion-client'
-
-const notion = new NotionAPI()
-
-const recordMap = await notion.getPage('067dd719a912471ea9a3ac10710e7fdf')
--- src/components/RenderNotionPage.js
+++
@@ -1,6 +0,0 @@
-import * as React from 'react'
-import { NotionRenderer } from 'react-notion-x'
-
-export default ({ recordMap }) => (
-  <NotionRenderer recordMap={recordMap} fullPage={true} darkMode={false} />
-)

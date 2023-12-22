faa5cd49bf7c29d85d4b9e4d15069fb16b8a2ff7
Justin Tam <tamtzekin@gmail.com>
2023-12-22 15:59:27 +0000

    neat

--- src/components/ClaimA.js
+++ src/components/ClaimA.js
@@ -1,17 +1,20 @@
 import logo from '../logo.svg';
-import React from 'react';
-import { useTable, useSortBy, useGlobalFilter, useFilters } from 'react-table';
+
+import React, { useMemo, useState} from 'react';
 import { Link } from 'react-router-dom';
+import { useTable, useSortBy, useGlobalFilter, useFilters } from 'react-table';
+
 import './ClaimPage.css';
 import '../App.css';
+
 import VideoPlayer from './VideoPlayer';
 import NavBar from './NavBar';
 import MobileMenu from './MobileMenu';
 import GlobalFilter from './GlobalFilter';
 
 
-function ClaimA() {
-    const data = React.useMemo(
+export default function ClaimA() {
+    const data = useMemo(
         () => [
             {
                 date: '10 Oct 2023',
@@ -22,9 +25,9 @@ function ClaimA() {
                 description: {
                     summary: 'Babies and toddlers were found with their “heads decapitated” in the Kfar Aza kibbutz',
                     summaryClass: 'expand-text',
-                    details: 'A spokesperson for the Israeli state’s prime minister claimed babies and toddlers were found with their “heads decapitated” in the Kfar Aza kibbutz after Hamas fighters entered occupied Palestine on October 7, 2023.<br /><br />Nicole Zedeck, a reporter with Tel Aviv-based news channel i24, interviewed Israeli occupation force soldier David Ben Zion who made the same claim on October 10, 2023. He said: “They cut heads of children, they cut heads of women.”',
+                    details: 'A spokesperson for the Israeli state’s prime minister claimed babies and toddlers were found with their “heads decapitated” in the Kfar Aza kibbutz after HHH fighters entered occupied Palestine on October 7, 2023.<br /><br />Nicole Zedeck, a reporter with Tel Aviv-based news channel i24, interviewed Israeli occupation force soldier David Ben Zion who made the same claim on October 10, 2023. He said: “They cut heads of children, they cut heads of women.”',
                 },
-                sourceText: 'i24NEWS: ‘Horror scenes at kibbutz liberated from Hamas’',
+                sourceText: 'i24NEWS: ‘Horror scenes at kibbutz liberated from HHH',
                 sourceLink: 'https://archive.ph/QeCXs',
                 videoLink: '/files/i24NEWS_10Oct.mp4',
             },
@@ -38,9 +41,9 @@ function ClaimA() {
                 description: {
                     summary: 'CNN reporter Nic Robertson claims babies’ heads cut off',
                     summaryClass: 'expand-text',
-                    details: 'Nic Robertson, a CNN reporter, claimed Palestinian resistance fighters carried out “ISIS-style executions” by “cutting the heads off of people” including babies and killing their pets.<br /><br />“Men, women, children, hands bound, shot, executed, heads cut.”',
+                    details: 'Nic Robertson, a CNN reporter, claimed Palestinian resistance fighters carried out III-style executions” by “cutting the heads off of people” including babies and killing their pets.<br /><br />“Men, women, children, hands bound, shot, executed, heads cut.”',
                 },
-                sourceText: 'CNN: ‘IDF, women, toddlers, elderly “butchered” by Hamas in Kfar Aza’',
+                sourceText: 'CNN: ‘IDF, women, toddlers, elderly “butchered” by HHH in Kfar Aza’',
                 sourceLink: 'https://archive.ph/XRmh4',
                 videoLink: '/files/CNN_NicRobertson_10Oct.mp4',
             },
@@ -86,171 +89,166 @@ function ClaimA() {
                 sources: 'Source 1',
             },
 
-
-            {
-                date: '',
-                claim: '',
-                claimClass: 'claim-tag',
-                description: {
-                    summary: '',
-                    summaryClass: '',
-                    details: '',
-                },
-                sources: 'Source 1',
-            },
-
               ],
             []
     )
 
+console.log("data" + data);
 
-const columns = React.useMemo(
-    () => [
+
+    const columns = useMemo(
+        () => [
+            // Properties
+            {
+                Header: 'Date',
+                accessor: 'date',
+                sortType: (rowA, rowB, columnId) => {
+                const dateA = new Date(rowA.values[columnId]);
+                const dateB = new Date(rowB.values[columnId]);
+                return dateA.getTime() - dateB.getTime();
+            },
+
+                Cell: ({ cell }) => (
+                    <div style={{ textAlign: 'center' }}>
+                        {cell.value}
+                    </div>
+                ),
+            },
+
+            {
+                Header: 'Claim',
+                accessor: (row) => row.claim.claimText,
+                Cell: ({ row }) => (
+                    <span className={row.original.claim.claimClass}>
+                        {row.original.claim.claimText}
+                    </span>
+                ),
+            },
+
+            {
+                Header: 'Description',
+                accessor: 'description',
+                Cell: ({ value }) => (
+                    <div style={{maxWidth:700}}>
+                        <details>
+                            <summary>{value.summary}<span className={value.summaryClass}></span></summary>
+                            <div dangerouslySetInnerHTML={{ __html: value.details }} />
+                        </details>
+                    </div>
+                ),
+            },
+
+
+            {
+                Header: 'Sources',
+                accessor: 'sourceText',
+                Cell: ({ row }) => (
+                    <VideoPlayer videoLink={row.original.videoLink}>
+                        <a href={row.original.sourceLink} target="_blank" rel="noreferrer">
+                            <span className="play-arrow"></span>
+                            {row.original.sourceText}
+                        </a>
+                    </VideoPlayer>
+                ),
+            },
+
+            ],
+            []
+        );
+
+        console.log("columns:" + columns);
+
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+        setGlobalFilter,
+        state,
+    } = useTable(
         {
-            Header: 'Date',
-            accessor: 'date',
-            sortType: (rowA, rowB, columnId) => {
-            const dateA = new Date(rowA.values[columnId]);
-            const dateB = new Date(rowB.values[columnId]);
-            return dateA.getTime() - dateB.getTime();
+            columns,
+            data,
+            initialState: {
+                sortBy: [{ id: 'date', desc: false }],
+            },
+            disableSortRemove: true,
         },
+            useFilters,
+            useGlobalFilter,
+            useSortBy
+        );
 
-            Cell: ({ cell }) => (
-                <div style={{ textAlign: 'center' }}>
-                    {cell.value}
-                </div>
-            ),
-        },
-    
+        const { globalFilter } = state
+        console.log("Search for: " + globalFilter);
 
-        {
-            Header: 'Claim',
-            accessor: (row) => row.claim.claimText,
-            Cell: ({ row }) => (
-                <span className={row.original.claim.claimClass}>
-                    {row.original.claim.claimText}
-                </span>
-            ),
-        },
-
-
-        {
-            Header: 'Description',
-            accessor: 'description',
-            Cell: ({ value }) => (
-                <div style={{maxWidth:700}}>
-                    <details>
-                        <summary>{value.summary}<span className={value.summaryClass}></span></summary>
-                        <div dangerouslySetInnerHTML={{ __html: value.details }} />
-                    </details>
-                </div>
-            ),
-        },
-
-
-        {
-            Header: 'Sources',
-            accessor: 'sourceText',
-            Cell: ({ row }) => (
-                <VideoPlayer videoLink={row.original.videoLink}>
-                    <a href={row.original.sourceLink} target="_blank" rel="noreferrer">
-                        <span className="play-arrow"></span>
-                        {row.original.sourceText}
-                    </a>
-                </VideoPlayer>
-            ),
-        },
-
-          ],
-        []
-    );
-
-const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-    setGlobalFilter,
-    state,
-} = useTable(
-    {
-        columns,
-        data,
-        initialState: {
-            sortBy: [{ id: 'date', desc: false }],
-        },
-        disableSortRemove: true,
-    },
-        useFilters,
-        useGlobalFilter,
-        useSortBy
-    );
-
-    const { globalFilter } = state
-
-return (
-    <>
-        <span class="claim-header-container">
-            <MobileMenu />
-            <Link to="/claims"><div className="arrow">←</div></Link>
-            <h1 class="claim-heading">Claim: 40 beheaded babies</h1>
-            <NavBar />
-        </span>
-    
-    <GlobalFilter filter={globalFilter} setFilter={setGlobalFilter} />
-    
-    <div>
-        <table {...getTableProps()}>
-        <thead>
-            {headerGroups.map((headerGroup) => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                    {headerGroup.headers.map((column) => (
-
-                <th
-                    {...column.getHeaderProps(
-                        column.id === 'date' ? column.getSortByToggleProps() : {}
-                    )}
-
-                    style={{
-                        color: 'black',
-                        cursor: column.id === 'date' ? 'pointer' : 'auto',
-                    }}
-                >
-                    {column.render('Header')}
-                    {column.id === 'date' && column.isSorted ? (column.isSortedDesc ? '▼' : '▲') : ''}
-                </th>
+    return (
+        <>
+            <span class="claim-header-container">
+                <MobileMenu />
+                <Link to="/claims"><div className="arrow">←</div></Link>
+                <h1 class="claim-heading">Claim: 40 beheaded babies</h1>
+                <NavBar />
+            </span>
+        
+        {/* render filter search bar  */}
+            <GlobalFilter filter={globalFilter || ''} setFilter={setGlobalFilter} />
+        
+        <div>
+            <table {...getTableProps()}>
+            <thead>
                 
-              ))}
-            </tr>
-          ))}
-        </thead>
-            
-            <tbody {...getTableBodyProps()}>
-                {rows.map((row) => {
-                    prepareRow(row);
-                    return (
-                <tr {...row.getRowProps()}>
-                    {row.cells.map((cell) => (
-                    <td
-                        {...cell.getCellProps()}
+                {/* Map the headers out */}
+                {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+
+                // Add sorting funct to the Date column
+                    <th
+                        {...column.getHeaderProps(
+                            column.id === 'date' ? column.getSortByToggleProps() : {}
+                        )}
+
                         style={{
-                            padding: '10px',
-                            border: 'solid 1px gray',
-                            overflow: 'hidden',
+                            color: 'black',
+                            cursor: column.id === 'date' ? 'pointer' : 'auto',
                         }}
                     >
-                        {cell.render('Cell')}
-                    </td>
+                        {column.render('Header')}
+                        {column.id === 'date' && column.isSorted ? (column.isSortedDesc ? '▼' : '▲') : ''}
+                    </th>
                 ))}
                 </tr>
-            );
-          })}
-            </tbody>
-        </table>
-    </div>
-    </>
-  );
-}
+            ))}
+            </thead>
+                
+                {/* fill all the body cell props */}
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                        prepareRow(row);
 
-export default ClaimA;
+                        return (
+                    <tr {...row.getRowProps()}>
+                        {row.cells.map((cell) => (
+                        <td
+                            {...cell.getCellProps()}
+                            style={{
+                                padding: '10px',
+                                border: 'solid 1px gray',
+                                overflow: 'hidden',
+                            }}
+                        >
+                            {cell.render('Cell')}
+                        </td>
+                    ))}
+                    </tr>
+                );
+            })}
+                </tbody>
+            </table>
+        </div>
+        </>
+    );
+}

6d966ca3f6ca942d4c42b1c9c0662e088ffbb790
Justin Tam <tamtzekin@gmail.com>
2023-12-22 22:37:14 +0000

    delete unused

---
+++ src/components/.VideoPlayer_tap handling.js
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
+const VideoPlayer = ({ videoLink, children }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  let hoverTimeout;
+
+  const openModal = () => {
+    hoverTimeout = setTimeout(() => {
+      setIsModalOpen(true);
+    }, 100);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleMouseLeave = () => {
+    clearTimeout(hoverTimeout);
+    closeModal(); // Close the modal when mouse leaves the link
+  };
+
+  const handleTouchStart = () => {
+    // Open the modal on touch start (for mobile devices)
+    openModal();
+  };
+
+
+  return (
+    <>
+      <a 
+        onMouseEnter={openModal} 
+        onMouseLeave={handleMouseLeave}
+        onTouchStart={handleTouchStart}
+      >
+        {children}
+      </a>
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Video Modal"
+        style={{
+          content: {
+            top: 'auto',
+            left: 'auto',
+            right: 0,
+            transform: 'none',
+            border: 'none',
+            background: 'none',
+            padding: 0,
+            width: '80%',
+            maxWidth: '400px',
+            maxHeight: '80%',
+            overflow: 'hidden',
+            animationFillMode: 'forwards',
+            position: 'absolute',
+            zIndex: 2,
+          },
+          overlay: {
+            backgroundColor: 'rgba(0, 0, 0, 0)',
+            animationFillMode: 'forwards',
+            zIndex: 1,
+          },
+        }}
+      >
+        <video
+          autoPlay
+          width="100%"
+          height="100%"
+          style={{
+            position: 'relative',
+            zIndex: 3,
+          }}
+          onEnded={closeModal}
+        >
+          <source src={videoLink} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </Modal>
+    </>
+  );
+};
+
+export default VideoPlayer;

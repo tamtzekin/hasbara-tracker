070c644f33ad1fb8a6a9ee2dc1a6af85a43b0632
Justin Tam <tamtzekin@gmail.com>
2023-12-28 15:26:00 +0000

    as

--- src/components/.Tracker - mobile view rendered without table, search not working on mob.js
+++ src/components/.Tracker - mobile view rendered without table, search not working on mob.js
@@ -271,93 +271,38 @@ export default function Tracker() {
         {/* MOBILE VIEW*/}
         {/* {isMobileView && renderMobileData()} */}
         {isMobileView && (
-        <>
-        <div class="search-bar-container">
-        <SearchBar filter={globalFilter || ''} setFilter={setGlobalFilter} />
-    </div>
-
-
-        <div class="tracker-container">
-            {rows.length === 0 ? (
                 <>
-                <div class="no-results-text">No results found. Try searching a different word or phrase.</div><div class="how-to">
-                How to use the tracker:
-                <ul>
-                <li>Use the search bar to look up words, phrases or claims (eg. ‘beheaded babies’, ‘hospital’, ‘al-shifa’).</li>
-                <li>You can also search by Claim / Debunk / Context by searching 'claim', 'debunk' or 'context'</li>
-                <li>Click ‘Date ▲’ to change the order of events (desktop only)</li>
-                <li>Hover over a ▷ source  to preview video (If you’re on your phone, tap + hold the link to preview). Click anywhere to close</li>
-                <li>Click each Source to open an archived link</li>
-                <li>Click + and ⎯ to show more or less text</li>
-                </ul>
-            </div>
-            </>
-            ) : (
+                <div class="search-bar-container">
+                    <SearchBar filter={globalFilter || ''} setFilter={setGlobalFilter} />
+                </div>
 
-            <table {...getTableProps()}>
-            <thead>
-                {/* Map the headers out */}
-                {headerGroups.map((headerGroup) => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map((column) => (
+                <div className="mobile-claims-display">
+                    {mappedData.map((mappedItem) => (
+                        <div key={mappedItem.id}>
 
-                // Add sorting function to the Date column
-                    <th
-                        {...column.getHeaderProps(
-                            column.id === 'date' ? column.getSortByToggleProps() : {}
-                        )}
+                        <div class="claim-mobile-summary">
+                            <span className={`mapped-item ${mappedItem.claimClass}`}>{mappedItem.claimText}</span> 
+                            <span className='claim-type-label'>{mappedItem.claimTag}</span>
+                            <span className='date'>{mappedItem.date}</span>
+                        </div>
 
-                        style={{
-                            color: 'black',
-                            cursor: column.id === 'date' ? 'ns-resize' : 'auto',
-                        }}
-                    >
-                        {column.render('Header')}
-                        {column.id === 'date' && column.isSorted ? (column.isSortedDesc ? ' ▼' : ' ▲') : ''}
-                    </th>
-                ))}
-                </tr>
-            ))}
-            </thead>
-                
-            {/* Fetch + fill out all the body cell props */}
-            <tbody {...getTableBodyProps()}>
-                {rows.map((row) => {
-                    prepareRow(row);
+                        <div class="claim-mobile-details">
+                            <span class="claim-mobile-details-heading">Details:</span><br />
+                            <details><summary>{mappedItem.summary}<span className={`${mappedItem.expandButton}`}></span></summary>
+                            <div dangerouslySetInnerHTML={{ __html: mappedItem.details }} />
+                            </details>
+                        </div>
 
-                    return (
-                        <tr 
-                            {...row.getRowProps()}>
-                            {row.cells.map((cell, index) => (
-                                <td
-                                    {...cell.getCellProps()}
-                                        className={`custom-cell-${index}`} // gives each cell its own class, for styling
-                                        style={{
-                                            padding: '20px',
-                                            borderBottom: 'solid 1px gray',
-                                            overflow: 'hidden',
-                                            
-                                            // Fix the size of each column
-                                            width:
-                                                index === 0 ? '30px' :
-                                                index === 1 ? '80px' :
-                                                index === 2 ? '20px' :
-                                                index === 3 ? '600px' :
-                                                '130px',
-                                        }}
-                                >
-                                    {cell.render('Cell')}
-                                </td>
-                            ))}
-                        </tr>
-                    );
-                })}
-            </tbody>
-            </table>
-            )}
-            </div>
-
-            </>
+                        <div class="claim-mobile-source">
+                            <span class="claim-mobile-source-heading">Source:</span><br /><br />
+                            <a href={mappedItem.sourceLink} target="_blank" rel="noreferrer">
+                                {mappedItem.sourceName}
+                            </a>
+                        </div>
+                    </div>
+                    ))}     
+                </div>
+                </>
                 
         )}
         

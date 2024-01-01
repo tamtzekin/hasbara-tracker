3dd64983e9edc3574a0662ca79b37d7f39a1bb98
Justin Tam <tamtzekin@gmail.com>
2024-01-01 21:55:55 +0000

    data formatting

--- src/components/data.js
+++ src/components/data.js
@@ -222,7 +222,7 @@ const data = [
         description: {
             summary: 'IOF spokesperson says “relatively confident” that babies were beheaded',
             summaryClass: 'expand-text',
-            details: 'In a video shared by the official IDF account (Israeli occupation forces), Israeli occupation force spokesperson Jonathon Conricus claimed Palestinian resistance fighters had “likely” carried out decapitations of babies in the Be’eri kibbutz.<br />“We got very very disturbing reports that came from the ground that there were babies that had been beheaded… I think we can now say with relative confidence that unfortunately this is what happened in Be’eri.”',
+            details: 'In a video shared by the official IDF account (Israeli occupation forces), Israeli occupation force spokesperson Jonathon Conricus claimed Palestinian resistance fighters had “likely” carried out decapitations of babies in the Be’eri kibbutz.<br /><br />“We got very very disturbing reports that came from the ground that there were babies that had been beheaded… I think we can now say with relative confidence that unfortunately this is what happened in Be’eri.”',
         },
         sourceText: 'Daily Mail',
         sourceLink: 'https://www.youtube.com/watch?v=Wsbd_dV8Xf8',

1badf7c9a071542455e810d5b9be5096322dd804
Justin Tam <tamtzekin@gmail.com>
2024-02-02 16:41:02 +1100

    text-to-speech accessibility
    when using text-to-speech, warns users links are opening in a new tab

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -96,7 +96,8 @@ export default function Tracker() {
                                 <VideoPlayer key={index} videoPreviewLink={source.videoPreviewLink}>
                                     <div key={index} class="source" style={{ marginBottom: '8%' }}>
 
-                                        <a href={source.sourceLink} target="_blank" rel="noreferrer">
+                                        <a href={source.sourceLink} target="_blank" rel="noreferrer"
+                                        aria-hidden="true">
                                             {/* If there's a video preview available, show a play button icon */}
                                             {source.videoPreviewLink && (
                                                 <>
@@ -112,12 +113,18 @@ export default function Tracker() {
                                             )}
 
                                             <span dangerouslySetInnerHTML={{ __html: source.sourceText }} />
+
+                                            {/* Text-to-Speech only message to tell users it opens in new tab */}
+                                            <span class="visually-hidden">Opens in new tab</span>
                                         </a>
 
                                         {source.archiveLink && ( 
-                                                <a href={source.archiveLink} target="_blank" rel="noreferrer">
+                                                <a href={source.archiveLink} target="_blank" rel="noreferrer"
+                                                aria-hidden="true">
                                                     &nbsp;<div class="archive-link">(archive)</div>
-                                                </a>
+                                                {/* Text-to-Speech only message to tell users it opens in new tab */}
+                                                <span class="visually-hidden">Opens in new tab</span>
+                                                </a>    
                                             )}
                                     </div>
                                     

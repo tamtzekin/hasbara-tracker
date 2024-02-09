11c8db95411470052b1df814966c48513db2d0c7
Justin Tam <tamtzekin@gmail.com>
2024-02-09 14:13:19 +1100

    added fade effect to expandable homepage text

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -21,7 +21,9 @@ const Homepage = () => {
             <span class="column">
             <details>
             <summary><h2>About us<span class="expand-text"></span></h2></summary>
+                <article>
                 <span className="home-text">We’re working on documenting and debunking claims made by the Israeli regime during its most recent — and unprecedented — genocidal campaign in Gaza since 7 Oct, 2023. The Israeli settler-colony continues to spread disinformation to manufacture consent for ethnic cleansing and land theft, as it has since the Nakba. Our aim is to centralise this information.<br /><br /></span>
+                </article>
                 </details>
 
                 {/* <a href="https://instagram.com/hasbaratracker" alt="Link to Instagram account @hasbaratracker" target="_blank" rel="noreferrer">@hasbaratracker</a> */}
@@ -30,8 +32,10 @@ const Homepage = () => {
 
                 <details>
                 <summary><h2>What is hasbara?<span class="expand-text"></span></h2></summary>
+                <article>
                 <span className="hasbara-about">‘Hasbara’ is a Hebrew term that translates to ‘explanation’ or ‘public relations’ in English. It refers to public relations efforts by the Israeli settler project aimed at promoting a positive image of itself by whitewashing its military occupation, apartheid system and ongoing dispossession of Palestine.<br />
                 <br /> Hasbara includes various forms of communication, such as media outreach, social media campaigns, and other strategies to shape public opinion in favour of the Israeli state, and involves manipulation of information, spreading propaganda, and stifling dissenting voices.</span>
+                </article>
                 </details>
                 </span>
 

f26e1cacdbbc36bbe9736113578ff47b9e382f7a
Justin Tam <tamtzekin@gmail.com>
2024-02-11 19:15:25 +1100

    reordered footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,8 @@ import '../App.css';
 const Footer = () => {
     return (
         <span class="footer">
-            <li><Link to="/volunteer">Volunteer</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
             <li><Link to="/newsletter">Newsletter</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
             <li><Link to="http://instagram.com/hasbaratracker" target="_blank">Instagram</Link></li>
         </span>
     )

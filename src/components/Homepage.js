98756aad1e9b89bcec9ebbab2f7629ebde8e9cee
Justin Tam <tamtzekin@gmail.com>
2024-02-14 21:18:59 +0000

    some spacing to mobile view of claims coming soon

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -78,14 +78,16 @@ const Homepage = () => {
 
             <h2>Claims</h2>
 
+            <p>
             <em>Coming soon:</em> Forty beheaded babies
-                <br /><br />
-                
-                <em>Coming soon:</em> Al-Ahli Hospital was attacked by Palestinian rockets, not Israeli forces
-                <br /><br />
+                </p>
 
+                <p>
+                <em>Coming soon:</em> Al-Ahli Hospital was attacked by Palestinian rockets, not Israeli forces
+                </p>
+                <p>
                 <em>Coming soon:</em> Israeli state offered fuel to Al-Shifa Hospital and it was refused by Hamas
-                <br /><br />
+                </p>
 
                 <br />
                 <br />

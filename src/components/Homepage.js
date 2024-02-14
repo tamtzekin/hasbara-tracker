b9f0ca80ecc12c2fb6b7f1ba73ba8d2095243d0b
Justin Tam <tamtzekin@gmail.com>
2024-02-14 21:16:18 +0000

    removed claims from mobile view

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -32,12 +32,6 @@ const Homepage = () => {
 
         <span class="column">
             <h2>Claims</h2>
-                {/* <div class="claim-link"><Link to="/forty-beheaded-babies">Forty beheaded babies</Link></div><br /> */}
-                
-                {/* <div class="claim-link"><Link to="/al-ahli-attacked">Al-Ahli Hospital was attacked by Palestinian rockets, not Israeli forces</Link></div><br /> */}
-                
-                {/* <div class="claim-link"><Link to="/al-shifa-fuel">Israeli state offered fuel to Al-Shifa Hospital and it was refused by Hamas</Link></div><br /> */}
-
                 <em>Coming soon:</em> Forty beheaded babies
                 <br /><br />
                 
@@ -83,13 +77,15 @@ const Homepage = () => {
             </details>
 
             <h2>Claims</h2>
-                <div class="claim-link"><Link to="/forty-beheaded-babies">Forty beheaded babies</Link></div><br />
-                
-                <div class="claim-link"><Link to="/al-ahli-attacked">Al-Ahli Hospital was attacked by Palestinian rockets, not Israeli forces</Link></div><br />
-                
-                <div class="claim-link"><Link to="/al-shifa-fuel">Israeli state offered fuel to Al-Shifa Hospital and it was refused by Hamas</Link></div><br />
 
-                <em>Coming soon:</em> xxxxxx
+            <em>Coming soon:</em> Forty beheaded babies
+                <br /><br />
+                
+                <em>Coming soon:</em> Al-Ahli Hospital was attacked by Palestinian rockets, not Israeli forces
+                <br /><br />
+
+                <em>Coming soon:</em> Israeli state offered fuel to Al-Shifa Hospital and it was refused by Hamas
+                <br /><br />
 
                 <br />
                 <br />

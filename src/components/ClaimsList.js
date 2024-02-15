809a831e5c915b6ee7166339e5c916a626bafa7c
Justin Tam <tamtzekin@gmail.com>
2024-02-15 19:26:07 +0000

    restyled coming soon claims

--- src/components/ClaimsList.js
+++ src/components/ClaimsList.js
@@ -5,27 +5,27 @@ const ClaimsList = () => {
     return (
     <>
         <h2>Claims</h2>
-        <div className="claim-coming-soon">Coming soon: Forty beheaded babies</div>
+        <div className="claim-coming-soon"><em>Coming soon:</em> Forty beheaded babies</div>
 
-        <div className="claim-coming-soon">Coming soon: Al-Ahli Hospital was attacked by Palestinian rockets, not Israeli forces</div>
+        <div className="claim-coming-soon"><em>Coming soon:</em> Al-Ahli Hospital was attacked by Palestinian rockets, not Israeli forces</div>
 
-        <div className="claim-coming-soon">Coming soon: Israeli state offered fuel to Al-Shifa Hospital and it was refused by Hamas</div>
+        <div className="claim-coming-soon"><em>Coming soon:</em> Israeli state offered fuel to Al-Shifa Hospital and it was refused by Hamas</div>
         
-        <div className="claim-coming-soon">Coming soon: Makeup used in Gaza to fake injuries</div>
+        <div className="claim-coming-soon"><em>Coming soon:</em> Makeup used in Gaza to fake injuries</div>
 
-        <div className="claim-coming-soon">Coming soon: Israeli state denies striking Al-Shifa hospital</div>
+        <div className="claim-coming-soon"><em>Coming soon:</em> Israeli state denies striking Al-Shifa hospital</div>
 
-        <div className="claim-coming-soon">Coming soon: Hamas tunnel found at the Qatari Hospital</div>
+        <div className="claim-coming-soon"><em>Coming soon:</em> Hamas tunnel found at the Qatari Hospital</div>
 
-        <div className="claim-coming-soon">Coming soon: Israeli state denies ordering World Health Organisation to remove medical supplies from its warehouse </div>
+        <div className="claim-coming-soon"><em>Coming soon:</em> Israeli state denies ordering World Health Organisation to remove medical supplies from its warehouse </div>
 
-        <div className="claim-coming-soon">Coming soon: Gaza’s electricity not cut off because ‘Palestinians are using phones’</div>
+        <div className="claim-coming-soon"><em>Coming soon:</em> Gaza’s electricity not cut off because ‘Palestinians are using phones’</div>
 
-        <div className="claim-coming-soon">Coming soon: Israeli state denies using white phosphorus munitions in Gaza</div>
+        <div className="claim-coming-soon"><em>Coming soon:</em> Israeli state denies using white phosphorus munitions in Gaza</div>
 
-        <div className="claim-coming-soon">Coming soon: There are no churches in Gaza</div>
+        <div className="claim-coming-soon"><em>Coming soon:</em> There are no churches in Gaza</div>
 
-        <div className="claim-coming-soon">Coming soon: Hostage gave birth in captivity</div>
+        <div className="claim-coming-soon"><em>Coming soon:</em> Hostage gave birth in captivity</div>
         </>
     )
 }

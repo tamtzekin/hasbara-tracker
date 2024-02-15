d3471bf7ab887a619ca48b38e65c8871b52211a8
Justin Tam <tamtzekin@gmail.com>
2024-02-15 16:13:30 +0000

    svg logo, as a component

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Logo = () =>{
+    return (
+        <>
+            <div className="logo">
+                <img src="https://files.hasbaratracker.com/hasbara_tracker_logo_transparent.svg" alt="Hasbara Tracker, in a pixellated font"></img>
+            </div>
+        </>
+    );
+}
+
+export default Logo;

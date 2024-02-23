393e0b3a1fca1b0d64b502abc04a7d9f17fd1615
Justin Tam <tamtzekin@gmail.com>
2024-02-23 11:51:57 +0000

    installed Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'],  
+        theme: {
+            colors: {
+                'grey': '',
+                'grey-light': '',
+                'green-light': '',
+            },
+        },
+    fontFamily: {
+        sans: ['Helvetica', 'sans-serif'],
+    }
+}

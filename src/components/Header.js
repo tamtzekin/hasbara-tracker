89adf35e088ea02468dfe2b3920a7e30618bfb4a
Justin Tam <tamtzekin@gmail.com>
2024-02-24 13:41:19 +0000

    aligned header and footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,22 +18,22 @@ const Header = () =>{
         
         <div className="nav-links">
             {/* {location.pathname !== '/about' && location.pathname !== '/' && ( */}
-                <li>
+                <li className="undotted">
                     <NavLink to="/">Claims</NavLink>
                 </li>
             {/* )} */}
 
             {/* {location.pathname !== '/submit-claim' && ( */}
-            <li>
+            <li className="undotted">
                 <NavLink to="/submit-claim">Submit a claim</NavLink>
             </li>
             {/* )} */}
 
-            <li>
+            <li className="undotted">
                 <NavLink to="/volunteer">Volunteer</NavLink>
             </li>
 
-            <li>
+            <li className="undotted">
                 <NavLink to="/about">About</NavLink>
             </li>
         </div>

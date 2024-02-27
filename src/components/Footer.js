89073b0e086226d4121962ff21a27144c2327f2c
Justin Tam <tamtzekin@gmail.com>
2024-02-27 01:03:30 +0000

    Footer not too close to edge on Safari, email sign up text colour

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,11 +9,11 @@ const Footer = () => {
       <footer className="flex-col bg-neutral-900 text-white sm:mt-0 mt-5">
         {/* To centre within column, add: flex items-center */}
         
-        <div className="mx-auto pt-6">
+        <div className="mx-auto pt-20">
           {/* Newsletter sign-up form */}
           <div className="flex-wrap items-center justify-center gap-4 md:flex-row">
             {/* Newsletter sign-up container */}
-            <div className="mobile:mb-4 laptop:-ml-10 laptop:-mb-14 flex items-end">
+            <div className="mobile:mb-4 laptop:-ml-10 laptop:-mb-10 flex items-end">
               {/* Newsletter sign-up text and input field */}
               <div className="mobile:mb-2 laptop:flex laptop:mb-0.5 desktop:flex md:items-center">
                 <p className="text-grey-faded text-xs laptop:mt-2.5 laptop:mr-3">
@@ -22,15 +22,15 @@ const Footer = () => {
                 <div className="relative flex items-center">
                   <input
                     type="text"
-                    className="signup-input peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] text-neutral-200 text-grey-darker outline-none transition-all duration-200 ease-linear motion-reduce:transition-none"
+                    className="signup-input peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] text-xs leading-[1.6] text-neutral-200 text-grey-darkest outline-none transition-all duration-200 ease-linear motion-reduce:transition-none"
                     id="exampleFormControlInput1"
-                    placeholder="--------@-----.com"
+                    placeholder="------------@--------.com"
                   />
                 </div>
               </div>
 
               {/* 'Sign up' button md:mr-auto  */}
-              <div className="md:ml-1 flex items-center ml-2">
+              <div className="md:ml-2 flex items-center ml-2">
                 <button
                   type="submit"
                   className="btn-green sign-up-form"
@@ -41,20 +41,28 @@ const Footer = () => {
             </div>
 
             {/* Links */}
-            <ul className="mobile:items-center mobile:-ml-6 mobile:pt-5 laptop:justify-end laptop:ml-auto pt-3 flex flex-row mt-3 text-sm text-gray-500 dark:text-gray-400">
+            <ul className="mobile:items-center mobile:-ml-14 mobile:pt-5 mobile:justify-around laptop:justify-end laptop:ml-auto flex flex-row mt-3 text-sm">
               <li className="laptop:me-10">
                 <a href="https://www.paypal.com/donate/?hosted_button_id=44XADNYVGGMPS" target="_blank" rel="noreferrer" className="hover:underline">
                   Donate
                 </a>
               </li>
+
               <li className="laptop:me-10">
                 <a href="/contact" className="hover:underline">
                   Contact
                 </a>
               </li>
-              <li>
+
+              <li className="laptop:me-10">
                 <a href="https://www.instagram.com/hasbaratracker/" target="_blank" rel="noreferrer" className="hover:underline">
-                  Instagram
+                  IG
+                </a>
+              </li>
+
+              <li>
+                <a href="https://www.twitter.com/hasbaratracker/" target="_blank" rel="noreferrer" className="hover:underline">
+                  X
                 </a>
               </li>
             </ul>

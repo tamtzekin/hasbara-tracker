a97a6a97bb00388bd0d5f107ca90547f150b66d5
Justin Tam <tamtzekin@gmail.com>
2024-03-01 06:19:53 +0000

    (mobile) logo positioning

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Logo = () =>{
     return (
         <>
-            <div className="logo-container relative w-72 mobile:w-1/2 mobile:mt-16 mobile:ml-0">
+            <div className="logo-container relative w-72 mobile:w-52 mobile:mt-5 mobile:ml-0">
                 <Link>
                 <img src="https://files.hasbaratracker.com/ht-logo-with-slogan.svg" alt="In a pixellated font, the title reads: Hasbara Tracker - Debunking Israeli propaganda"></img>
                 </Link>

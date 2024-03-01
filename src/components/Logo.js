69bc85acdb98834d0c5436d8c140ac974bdb84f4
Justin Tam <tamtzekin@gmail.com>
2024-03-01 23:59:11 +0000

    fixed logo link to home

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -5,8 +5,8 @@ const Logo = () =>{
     return (
         <>
             <div className="logo-container relative w-72 mt-[0.35%] mobile:w-52 mobile:mt-16 mobile:ml-0">
-                <Link>
-                <img src="https://files.hasbaratracker.com/ht-logo-with-slogan.svg" alt="In a pixellated font, the title reads: Hasbara Tracker - Debunking Israeli propaganda"></img>
+                <Link to="/">
+                    <img src="https://files.hasbaratracker.com/ht-logo-with-slogan.svg" alt="In a pixellated font, the title reads: Hasbara Tracker - Debunking Israeli propaganda"></img>
                 </Link>
             </div>
         </>

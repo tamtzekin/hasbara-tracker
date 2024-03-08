7eb1d556a9e8537bffdec5d5e98690204049615a
Justin Tam <tamtzekin@gmail.com>
2024-03-08 02:16:55 +0000

    sticky header sits under video overlay

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -78,7 +78,7 @@ const VideoPlayer = ({ videoPreviewLink, children }) => {
                         overflow: 'hidden',
                         animationFillMode: 'forwards',
                         position: 'absolute',
-                        zIndex: 2,
+                        // zIndex: 2,
 
                         // On phone: Show modal at bottom of screen
                         ...(screenWidth <= 576 && {
@@ -103,7 +103,7 @@ const VideoPlayer = ({ videoPreviewLink, children }) => {
                     height="100%"
                     style={{
                         position: 'relative',
-                        zIndex: 3,
+                        zIndex: 50,
                     }}
                     onEnded={closeModal}
                 >

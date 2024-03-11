4b1e2d27e531a033d719f6df1d49506c55125e49
Justin Tam <tamtzekin@gmail.com>
2024-03-11 18:24:27 +0000

    move menu toggle up (40BB page)

--- src/components/ClaimFortyBeheadedBabies.js
+++ src/components/ClaimFortyBeheadedBabies.js
@@ -346,7 +346,7 @@ export default function ClaimFortyBeheadedBabies() {
             </div>
 
         {/* Mobile menu toggle */}
-            <span className="mobile:mt-[-0.5%]">
+            <span className="mobile:mt-[3%]">
                 <MobileMenu />
             </span>
 

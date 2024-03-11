4bfdae0cc3a6a3383f3dc0afb275d26210cb9179
Justin Tam <tamtzekin@gmail.com>
2024-03-11 18:07:41 +0000

    logo too high up. fixed

--- src/components/ClaimMakeup.js
+++ src/components/ClaimMakeup.js
@@ -346,7 +346,7 @@ export default function ClaimMakeup() {
             </div>
 
         {/* Mobile menu toggle */}
-            <span className="mobile:mt-[-0.5%]">
+            <span className="mobile:mt-[3%]">
                 <MobileMenu />
             </span>
 

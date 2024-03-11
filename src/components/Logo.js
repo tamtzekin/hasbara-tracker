d6af9fec39b30074dc90074069fa0b4d02db1e6a
Justin Tam <tamtzekin@gmail.com>
2024-03-11 01:55:54 +0000

    (mobile) stop claim summary from clashing with all claims data

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Logo = () =>{
     return (
         <>
-            <div className="logo-container relative w-72 mt-[0.35%] mobile:w-52 mobile:mt-[-3%] mobile:ml-0">
+            <div className="logo-container relative w-72 mt-[0.35%] mobile:w-52 mobile:mt-[-2%] mobile:ml-0">
                 <Link to="/">
                     <img src="https://files.hasbaratracker.com/ht-logo-with-slogan.svg" alt="In a pixellated font, the title reads: Hasbara Tracker - Debunking Israeli propaganda"></img>
                 </Link>

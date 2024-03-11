65fe551958922876503d59b4e2ce04eb654ce9a7
Justin Tam <tamtzekin@gmail.com>
2024-03-11 17:55:47 +0000

    (mobile) logo more space above

--- src/components/ClaimAlAhliAttacked.js
+++ src/components/ClaimAlAhliAttacked.js
@@ -346,7 +346,7 @@ export default function ClaimAlAhliAttacked() {
             </div>
 
         {/* Mobile menu toggle */}
-            <span className="mobile:mt-[-0.5%]">
+            <span className="mobile:mt-[3%]">
                 <MobileMenu />
             </span>
 

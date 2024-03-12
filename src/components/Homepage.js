b18bed57008f4765364bfe986d8795649c8654be
Justin Tam <tamtzekin@gmail.com>
2024-03-12 00:56:42 +0000

    disabled hardcoded metadata, tried Helmet on homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,33 +2,36 @@ import React from 'react';
 import Header from './Header';
 import ClaimsList from './ClaimsList';
 import Footer from './Footer';
-import { Helmet } from 'react-helmet-async';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 // import '../App.css';
 
 const Homepage = () => {
     return (
+        <HelmetProvider>
         <>
-        
         <Helmet>
+            {/* HTML meta tags */}
+            {/* Important metadata */}
+            <meta name="url" property="og:url" content="https://hasbaratracker.com/" />
+            <meta name="type" property="og:type" content="article" />
+            <meta name="site_name" property="og:site_name" content="Hasbara Tracker" />
             <title>Hasbara Tracker</title>
+            <meta name="title" property="og:title" content="Hasbara Tracker" />
+            <meta name="description" property="og:description" content="A database tracking and debunking fabrications by the Israeli state during its current genocidal campaign against Palestinians in Gaza since 7 October 2023." />
+            <meta name="image" property="og:image" content="https://files.hasbaratracker.com/htlogo_twittercard.jpg" />
             
-            {/* <!-- Important metadata --> */}
-            <meta name="og:title" content="Hasbara Tracker" />
-            <meta name="og:type" content="article" />
-            <meta name="og:image" content="https://files.hasbaratracker.com/htlogo_twittercard.jpg" />
-            <meta name="og:url" content="https://hasbaratracker.com" />
+            <meta property="twitter:image:alt" content="In a pixellated font, the title reads: Hasbara Tracker - Debunking Israeli propaganda" />
 
-            <meta name="og:description" content="A database tracking and debunking fabrications by the Israeli state during its current genocidal campaign against Palestinians in Gaza since 7 October 2023." />
-            <meta name="og:site_name" content="Hasbara Tracker" />
-            <meta name="twitter:image:alt" content="In a pixellated font, the title reads: Hasbara Tracker - Debunking Israeli propaganda" />
-
-            {/* <!-- Twitter link preview metadata --> */}
+            {/* Twitter link preview metadata */}
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:site" content="@hasbaratracker" />
-            <meta name="twitter:title" content="hasbaratracker.com" />
-            <meta name="twitter:description" content="We’re working on documenting and debunking claims made by the Israeli regime during its most recent — and unprecedented — genocidal campaign in Gaza since 7 Oct, 2023. The Israeli settler-colony continues to spread disinformation to manufacture consent for ethnic cleansing and land theft, as it has since the Nakba. Our aim is to centralise this information." />
+            <meta name="twitter:title" content="Hasbara Tracker" />
+            <meta name="twitter:creator" content="@hasbaratracker" />
+            <meta name="twitter:description" content="A database tracking and debunking fabrications by the Israeli state during its current genocidal campaign against Palestinians in Gaza since 7 October 2023." />
             <meta name="twitter:image" content="https://files.hasbaratracker.com/htlogo_twittercard.jpg" />
+            <meta property="twitter:url" content="https://hasbaratracker.com/" />
+            <meta property="twitter:domain" content="hasbaratracker.com" />
         </Helmet>
 
         <Header />
@@ -105,6 +108,7 @@ const Homepage = () => {
 
         <Footer />
     </>
+    </HelmetProvider>
     );
 };
 

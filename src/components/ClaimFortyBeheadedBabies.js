539cb9f8ea6b67b86ff2ebaaae1858c37dc849fb
Justin Tam <tamtzekin@gmail.com>
2024-03-21 18:09:58 +0000

    metadata set in its own component

--- src/components/ClaimFortyBeheadedBabies.js
+++ src/components/ClaimFortyBeheadedBabies.js
@@ -1,39 +1,46 @@
 import React, { useMemo, useEffect, useState } from 'react';
 import { useTable, useSortBy, useGlobalFilter, useFilters } from 'react-table';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from 'react-helmet-async';
 
-import './Tracker.css';
+// Page
 import '../App.css';
-
-import { data } from './data';
-import ClaimFilter from './ClaimFilter';
-import SearchBar from './SearchBar';
-import TrackerColumns from './TrackerColumns';
-
+import PageMetadata from './PageMetadata';
 import MobileMenu from './MobileMenu';
 import Logo from './Logo';
-import NavLinks from './NavLinks'
+import NavLinks from './NavLinks';
 import Footer from './Footer';
 import BackToTop from './BackToTop';
+
+// Tracker
+import './Tracker.css';
+import { data } from './data';
+import TrackerColumns from './TrackerColumns';
 import ClaimSummary from './ClaimSummary';
 import RenderTrackerDesktop from './RenderTrackerDesktop';
 import RenderTrackerMobile from './RenderTrackerMobile';
 
-
 export default function ClaimFortyBeheadedBabies() {    
+    const metadataProps = {
+        url: "https://hasbaratracker.com/forty-beheaded-babies",
+        title: "Claim: ‘Forty beheaded babies’",
+        description: "Israeli officials claimed up to 40 babies were decapitated in kibbutzes following Operation Al-Aqsa Flood on 7 October, 2023. Versions of this fabrication have been repeated by politicians and the media globally.",
+        twitterTitle: "Claim: ‘Forty beheaded babies’",
+        twitterDescription: "Israeli officials claimed up to 40 babies were decapitated in kibbutzes following Operation Al-Aqsa Flood on 7 October, 2023. Versions of this fabrication have been repeated by politicians and the media globally.",
+        twitterUrl: "https://hasbaratracker.com/forty-beheaded-babies",
+    };
+
     // defines claim tags for dropdown (ClaimFilter.js)
     const uniqueClaimTitles = useMemo(() => {
         const claimTitlesSet = new Set(data.map((item) => item.claimTitle));
         return Array.from(claimTitlesSet);
-        }, [data]);
+    }, [data]);
     
     const [selectedClaimTitle, setSelectedClaimTitle] = useState('');
-    
 
     // Set mobile/phone view dimensions
     const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 576);
 
-    // Force to render if <= 576, so the Sourrce links don't have the hovered video player attached.
+    // Force to render if <= 576, so the Source links don't have the hovered video player attached.
     const [forceRender, setForceRender] = useState(false);
     
     useEffect(() => {
@@ -52,7 +59,6 @@ export default function ClaimFortyBeheadedBabies() {
             window.removeEventListener('resize', handleResize);
         };
     }, []);
-
 
     // Pull all the claims data, change order based on desktop/mobile
     const columns = useMemo(() => {
@@ -86,87 +92,59 @@ export default function ClaimFortyBeheadedBabies() {
     // Sets state based on what is in the search box
     const { globalFilter } = state;
 
+    return (
+        <HelmetProvider>
+            <>
+                {/* Render page metadata */}
+                <PageMetadata {...metadataProps} />
 
-  return (
-    <HelmetProvider>
-    <>
-        <Helmet>
-            {/* HTML meta tags */}
-            {/* Important metadata */}
-            <meta name="url" property="og:url" content="https://hasbaratracker.com/forty-beheaded-babies" />
-            <meta name="type" property="og:type" content="article" />
-            <meta name="site_name" property="og:site_name" content="Claim: ‘Forty beheaded babies’" />
-            <meta name="site_name" property="og:site_name" content="Hasbara Tracker" />
-            <title>Claim: ‘Forty beheaded babies’</title>
-            <meta name="title" property="og:title" content="Claim: ‘Forty beheaded babies’" />
-            <meta name="description" property="og:description" content="Israeli officials claimed up to 40 babies were decapitated in kibbutzes following Operation Al-Aqsa Flood on 7 October, 2023. Versions of this fabrication have been repeated by politicians and the media globally." />
-            <meta name="image" property="og:image" content="https://files.hasbaratracker.com/htlogo_twittercard.jpg" />
-            
-            <meta property="twitter:image:alt" content="In a pixellated font, the title reads: Hasbara Tracker - Debunking Israeli propaganda" />
+                {/* Header (fixed) */}
+                <span className="header-container-fixed">
+                    <span className="flex">
+                        <Logo />
+                        <MobileMenu />
+                        <NavLinks />
+                    </span>
 
-            {/* Twitter link preview metadata */}
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:site" content="@hasbaratracker" />
-            <meta name="twitter:title" content="Claim: ‘Forty beheaded babies’" />
-            <meta name="twitter:creator" content="@hasbaratracker" />
-            <meta name="twitter:description" content="Israeli officials claimed up to 40 babies were decapitated in kibbutzes following Operation Al-Aqsa Flood on 7 October, 2023. Versions of this fabrication have been repeated by politicians and the media globally." />
-            <meta name="twitter:image" content="https://files.hasbaratracker.com/htlogo_twittercard.jpg" />
-            <meta property="twitter:url" content="https://hasbaratracker.com/forty-beheaded-babies" />
-            <meta property="twitter:domain" content="hasbaratracker.com" />
-        </Helmet>
+                    {/* Shows title and summary of the claim */}
+                    <ClaimSummary id={0} />
+                </span>
 
+                {/* Show desktop view of Tracker - as a table */}
+                {!isMobileView && (
+                    <RenderTrackerDesktop
+                        {...{
+                            headerGroups,
+                            getTableProps,
+                            getTableBodyProps,
+                            rows,
+                            prepareRow,
+                            globalFilter,
+                            setGlobalFilter,
+                            uniqueClaimTitles,
+                        }}
+                    />
+                )}
+                
+                {/* Show mobile view of Tracker table - as cards */}
+                {isMobileView && (
+                    <RenderTrackerMobile
+                        {...{
+                            headerGroups,
+                            getTableProps,
+                            getTableBodyProps,
+                            rows,
+                            prepareRow,
+                            globalFilter,
+                            setGlobalFilter,
+                            uniqueClaimTitles,
+                        }}
+                    />    
+                )}
 
-    {/* Header (fixed) */}
-    <span className="header-container-fixed">
-        <span className="flex">
-            <Logo />
-            <MobileMenu />
-            <NavLinks />
-        </span>
-
-        {/* Shows title and summary of the claim */}
-        <ClaimSummary id={0} />
-    </span>
-        
-    
-
-            {/* Show desktop view of Tracker - as a table */}
-            {!isMobileView && (
-                <RenderTrackerDesktop
-                    {...{
-                        headerGroups,
-                        getTableProps,
-                        getTableBodyProps,
-                        rows,
-                        prepareRow,
-                        globalFilter,
-                        setGlobalFilter,
-                        uniqueClaimTitles,
-                    }}
-                />
-            )}
-            
-            {/* Show mobile view of Tracker table - as cards */}
-            {isMobileView && (
-                <RenderTrackerMobile
-                    {...{
-                        headerGroups,
-                        getTableProps,
-                        getTableBodyProps,
-                        rows,
-                        prepareRow,
-                        globalFilter,
-                        setGlobalFilter,
-                        uniqueClaimTitles,
-                    }}
-                />    
-            )}
-
-        <BackToTop />
-        <Footer />
-    </>
-
-    </HelmetProvider>
-
+                <BackToTop />
+                <Footer />
+            </>
+        </HelmetProvider>
     );
 };
---
+++ src/components/PageMetadata.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+// This is set on each individual claim page
+const PageMetadata = ({
+    url,
+    title,
+    description,
+    twitterTitle,
+    twitterDescription,
+    twitterUrl,
+}) => {
+
+    // Metadata that stays the same per page
+    const type = "article";
+    const siteName = "Hasbara Tracker";
+    const image = "https://files.hasbaratracker.com/htlogo_twittercard.jpg";
+    const twitterImageAlt = "In a pixellated font, the title reads: Hasbara Tracker - Debunking Israeli propaganda";
+    const twitterCard = "summary_large_image";
+    const twitterSite = "@hasbaratracker";
+    const twitterCreator = "@hasbaratracker";
+    const twitterImage = "https://files.hasbaratracker.com/htlogo_twittercard.jpg";
+    const twitterDomain = "hasbaratracker.com";
+    
+    return (
+        <Helmet>
+            {/* OpenGraph metadata */}
+            <meta name="url" property="og:url" content={url} />
+            <meta name="type" property="og:type" content={type} />
+            <meta name="site_name" property="og:site_name" content={siteName} />
+            <title>{title}</title>
+            <meta name="title" property="og:title" content={title} />
+            <meta name="description" property="og:description" content={description} />
+            <meta name="image" property="og:image" content={image} />
+            
+            <meta property="twitter:image:alt" content={twitterImageAlt} />
+
+            {/* Twitter metadata */}
+            <meta name="twitter:card" content={twitterCard} />
+            <meta name="twitter:site" content={twitterSite} />
+            <meta name="twitter:title" content={twitterTitle} />
+            <meta name="twitter:creator" content={twitterCreator} />
+            <meta name="twitter:description" content={twitterDescription} />
+            <meta name="twitter:image" content={twitterImage} />
+            <meta property="twitter:url" content={twitterUrl} />
+            <meta property="twitter:domain" content={twitterDomain} />
+        </Helmet>
+    );
+};
+
+export default PageMetadata;

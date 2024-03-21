6e42563e5c6d7cca49071968aee162188728a95b
Justin Tam <tamtzekin@gmail.com>
2024-03-21 17:16:32 +0000

    tracker (desktop + mobile) as its own components

--- src/components/ClaimFortyBeheadedBabies.js
+++ src/components/ClaimFortyBeheadedBabies.js
@@ -1,15 +1,11 @@
 import React, { useMemo, useEffect, useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
 import { useTable, useSortBy, useGlobalFilter, useFilters } from 'react-table';
-
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import './Tracker.css';
 import '../App.css';
 
 import { data, summaries } from './data';
-// import VideoPlayer from './VideoPlayer';
-import Header from './Header';
 import ClaimFilter from './ClaimFilter';
 import SearchBar from './SearchBar';
 import TrackerColumns from './TrackerColumns';
@@ -19,9 +15,11 @@ import Logo from './Logo';
 import NavLinks from './NavLinks'
 import Footer from './Footer';
 import BackToTop from './BackToTop';
+import RenderTrackerDesktop from './RenderTrackerDesktop';
+import RenderTrackerMobile from './RenderTrackerMobile';
 
 
-export default function ClaimFortyBeheadedBabies() {
+export default function ClaimFortyBeheadedBabies() {    
     // defines claim tags for dropdown (ClaimFilter.js)
     const uniqueClaimTitles = useMemo(() => {
         const claimTitlesSet = new Set(data.map((item) => item.claimTitle));
@@ -143,161 +141,41 @@ export default function ClaimFortyBeheadedBabies() {
 
             {/* Show desktop view of Tracker - as a table */}
             {!isMobileView && (
-                <>
-                    <div className="search-bar-container">
-                        <ClaimFilter 
-                            claimTitles={uniqueClaimTitles} setGlobalFilter={setGlobalFilter} 
-                        />
-                        <SearchBar 
-                            filter={globalFilter || ''} setFilter={setGlobalFilter} 
-                        />
-                    </div>
-
-                    <div className="tracker-container">
-                        {/* Show 'how to' text when no results are found */}
-                        {rows.length === 0 ? (
-                            <>
-                                <div className="no-results-text">No results found. Try searching a different word or phrase.</div>
-                                <div className="how-to">
-                                    How to use the tracker:
-                                    <ul className="dotted">
-                                        <li className="dotted">Use the dropdown list to select a major claim and see all records of the claim.</li>
-                                        <li className="dotted">Use the search bar to look up words, phrases or topics (eg. ‘hospital’, ‘biden’, ‘al jazeera’)</li>
-                                        <li className="dotted">You can search by type of claim with ‘claim’, ‘debunk’ or ‘context’</li>
-                                        <li className="dotted">Click ‘Date ▲’ to change the order of events (desktop only)</li>
-                                        <li className="dotted">Hover over a source with <span className="icon-play"></span> to preview video (If you’re on your phone, tap + hold the link)</li>
-                                        <li className="dotted">Click each Source to open an archived link</li>
-                                        <li className="dotted">Click + and ⎯ to show more or less text</li>
-                                    </ul>
-                                </div>
-                            </>
-                        ) : (
-                            // Renders all claims data as a table
-                            <table {...getTableProps()}>
-                                <thead className="font-mono sticky top-0">
-                                    {headerGroups.map((headerGroup) => (
-                                        <tr {...headerGroup.getHeaderGroupProps()}>
-                                            {headerGroup.headers.map((column) => (
-                                                <th
-                                                    {...column.getHeaderProps(
-                                                        column.id === 'date' ? column.getSortByToggleProps() : {}
-                                                    )}
-                                                    style={{
-                                                        color: 'black',
-                                                        cursor: column.id === 'date' ? 'ns-resize' : 'auto',
-                                                    }}
-                                                >
-                                                    {/* 'Date' heading */}
-                                                    <span style={{ marginRight: '0.25rem'}}>
-                                                        {column.render('Header')}
-                                                    </span>
-
-                                                    {/* 'Date' asc/desc arrow */}
-                                                    {column.id === 'date' && column.isSorted ? (
-                                                        <span style={{ verticalAlign: 'middle' }}>
-                                                            {column.isSortedDesc ? '▼' : '▲'} 
-                                                        </span>
-                                                    ) : ''}                                                </th>
-                                            ))}
-                                        </tr>
-                                    ))}
-                                                        <div className="header-container-border"></div>
-
-                                </thead>
-                                
-                                <tbody {...getTableBodyProps()}>
-                                    {rows.map((row) => {
-                                        prepareRow(row);
-                                        return (
-                                            <tr {...row.getRowProps()}>
-                                                {row.cells.map((cell, index) => (
-                                                    <td
-                                                        {...cell.getCellProps()}
-                                                        style={{
-                                                            padding: '40px 40px 40px 0',
-                                                            borderBottom: '1px dashed #5e5e5e',
-                                                            overflow: 'visible',
-                                                            width: // Set fixed column widths
-                                                                index === 0 ? '20%' :
-                                                                index === 1 ? '9%' :
-                                                                index === 2 ? '36%' :
-                                                                index === 3 ? '7%' :
-                                                                '18%',
-                                                        }}
-                                                    >
-                                                        {cell.render('Cell')}
-                                                    </td>
-                                                ))}
-                                            </tr>
-                                        );
-                                    })}
-                                </tbody>
-                            </table>
-                        )}
-                    </div>
-                </>
+                <RenderTrackerDesktop
+                    {...{
+                        headerGroups,
+                        getTableProps,
+                        getTableBodyProps,
+                        rows,
+                        prepareRow,
+                        globalFilter,
+                        setGlobalFilter,
+                        uniqueClaimTitles,
+                    }}
+                />
             )}
-
             
             {/* Show mobile view of Tracker table - as cards */}
             {isMobileView && (
-                <>
-                    <div className="tools-background">
-                        <div className="search-bar-container">
-                            <ClaimFilter claimTitles={uniqueClaimTitles} setGlobalFilter={setGlobalFilter} />
-                            <SearchBar filter={globalFilter || ''} setFilter={setGlobalFilter} />
-                        </div>
-                    </div>
-
-                    <div className="tracker-container">
-                        {/* Show 'how to' text when no results are found */}
-                        {rows.length === 0 ? (
-                            <>
-                                <div className="no-results-text">No results found. Try searching a different word or phrase.</div>
-                                <div className="how-to">
-                                    How to use the tracker:
-                                    <ul>
-                                        <li>Use the dropdown list to select a major claim and see all records of the claim.</li>
-                                        <li>Use the search bar to look up words, phrases or topics (eg. ‘hospital’, ‘biden’, ‘al jazeera’)</li>
-                                        <li>You can search by type of claim with ‘claim’, ‘debunk’ or ‘context’</li>
-                                        <li>Click ‘Date ▲’ to change the order of events (desktop only)</li>
-                                        <li>Hover over a source with <span className="icon-play"></span> to preview video (If you’re on your phone, tap + hold the link)</li>
-                                        <li>Click each Source to open an archived link</li>
-                                        <li>Click + and ⎯ to show more or less text</li>
-                                    </ul>
-                                </div>
-                        </>
-
-                ) : (
-
-                    // Renders all claims data in divs, instead of a table
-                    <div {...getTableBodyProps()}>
-                        {rows.map((row) => {
-                            prepareRow(row);
-                            return (
-                                <div key={row.id} 
-                                    className="data-row" 
-                                    style={{ 
-                                        display: 'block',
-                                        alignItems: 'center' 
-                                    }}>
-                                    {row.cells.map((cell, index) => (
-                                        <div key={cell.column.id} className={`data-cell ${cell.column.Header.toLowerCase()}`}>
-                                            {cell.render('Cell')}
-                                        </div>
-                                    ))}
-                                </div>
-                            );
-                        })}
-                    </div>
-                )}
-            </div>
-        </>
-    )}
+                <RenderTrackerMobile
+                    {...{
+                        headerGroups,
+                        getTableProps,
+                        getTableBodyProps,
+                        rows,
+                        prepareRow,
+                        globalFilter,
+                        setGlobalFilter,
+                        uniqueClaimTitles,
+                    }}
+                />    
+            )}
 
         <BackToTop />
         <Footer />
     </>
+
     </HelmetProvider>
+
     );
-}
+};
